a99154493a2a5558a920dd72a89c2cd288bc93c8
Mato <Kittawat@icloud.com>
2023-12-18 20:27:37 +0800

    profile done

--- src/pages/profileLogin/index.tsx
+++ src/pages/profileLogin/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { User } from "../../components/User"
 import { persons } from "../../assets/constant"
 import Button from "../../components/Button"
+import { Link } from "react-router-dom"
 
 const ProfilePage = () => {
   return (
@@ -11,12 +12,14 @@ const ProfilePage = () => {
         <div className="flex gap-4 ">
           {persons.map((item, index) => {
             return (
-              <User
-                name={item.name}
-                imageSource={item.picture}
-                locked={item.lock}
-                key={`item.name-key-${index}`}
-              />
+              <Link to={`/homepage`}>
+                <User
+                  name={item.name}
+                  imageSource={item.picture}
+                  locked={item.lock}
+                  key={`item.name-key-${index}`}
+                />
+              </Link>
             )
           })}
         </div>
