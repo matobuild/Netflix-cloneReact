6cd5ff9caba108b88711d102bf1861157f6a6d03
Mato <Kittawat@icloud.com>
2024-01-10 10:13:50 +0800

    impliment ReactPagination

--- src/components/MovieCardFullPage/MovieCardFullPage.tsx
+++ src/components/MovieCardFullPage/MovieCardFullPage.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { ListServices } from "../../services/movieList"
 import { useMovieListStore } from "../../store/movieList"
 import { ListType } from "../../utils/constant"
 import { MovieDetail } from "../../interface/movieList"
 import { Link } from "react-router-dom"
 import Pagination from "../Pagination"
-import ReactPaginationCustom from "../../components/ReactPagination/ReactPagination"
 import { useMoviePageListStore } from "../../store/moviePageList"
+import ReactPaginate from "react-paginate"
+import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai"
+import { IconContext } from "react-icons"
+import "./MovieCardFullPage.css"
 
 type MovieCardProps = {
   list: ListType
@@ -16,52 +19,26 @@ type MovieCardProps = {
 const MovieCardFullPage = ({ list, name }: MovieCardProps) => {
   const { movie, setMovieList } = useMovieListStore()
   const { movie2, setMovieList2 } = useMoviePageListStore()
+  const [mvList, setMvList] = useState([])
+  const [totalPage, setTotalPage] = useState(0)
 
-  const getMovieDiscoveryData = async () => {
-    const data = await ListServices.getList(list)
-    console.log("data", data.data)
-
-    const data2 = await ListServices.getListForTHatPage(list, 2)
-    console.log("data2", data2)
-
-    if (data && data.data) {
-      setMovieList(list, {
-        data: data.data.results,
-        loading: false,
-        error: null,
-      })
-    }
-    if (data2 && data2.data) {
-      setMovieList2(list, {
-        data: data2.data,
-        loading: false,
-        error: null,
-      })
-    }
+  async function getFirstMovieList() {
+    const data2 = await ListServices.getListForTHatPage(list, 1)
+    setMvList(data2.data.results)
+    setTotalPage(data2.data.total_pages)
   }
 
   useEffect(() => {
-    getMovieDiscoveryData()
+    getFirstMovieList()
   }, [])
-  console.log("movieCard", list, movie)
+
   console.log("movieCard2", list, movie2)
 
   return (
     <div className="flex flex-col w-full justify-center ">
       <div className="text-3xl pt-4 pl-4">{name}</div>
       <div className="grid grid-cols-8  gap-y-4 pt-4 pb-9 pl-4">
-        {movie[list].data.map((res: MovieDetail) => (
-          <div key={`${list} movie ${res.title}`}>
-            <Link to={`/detail/${res.title} `} state={{ id: res.id }}>
-              <img
-                className="rounded-lg border-2 border-transparent hover:border-white"
-                src={`https://image.tmdb.org/t/p/w185${res.poster_path}`}
-                alt="movie_image"
-              />
-            </Link>
-          </div>
-        ))}
-        {movie2[list].data.results.map((res: MovieDetail) => (
+        {mvList.map((res: MovieDetail) => (
           <div key={`${list} movie ${res.title}`}>
             <Link to={`/detail/${res.title} `} state={{ id: res.id }}>
               <img
@@ -76,7 +53,33 @@ const MovieCardFullPage = ({ list, name }: MovieCardProps) => {
       <div className="flex justify-center pb-9">
         <Pagination />
       </div>
-      <ReactPaginationCustom />
+      <div className="App">
+        <ReactPaginate
+          containerClassName={"pagination"}
+          activeClassName={"active"}
+          pageClassName={"page-item"}
+          onPageChange={async (event) => {
+            console.log("page", event.selected)
+            const data2 = await ListServices.getListForTHatPage(
+              list,
+              event.selected + 1
+            )
+            setMvList(data2.data.results)
+          }}
+          breakLabel="..."
+          pageCount={20}
+          previousLabel={
+            <IconContext.Provider value={{ color: "#B8C1CC", size: "36px" }}>
+              <AiFillLeftCircle />
+            </IconContext.Provider>
+          }
+          nextLabel={
+            <IconContext.Provider value={{ color: "#B8C1CC", size: "36px" }}>
+              <AiFillRightCircle />
+            </IconContext.Provider>
+          }
+        />
+      </div>
     </div>
   )
 }
