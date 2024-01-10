d5e8f8b6271a0ffeb61ae9540aacd88c18387f18
Mato <Kittawat@icloud.com>
2024-01-10 10:13:30 +0800

    implement ReactPagination

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -14,14 +14,6 @@ const HomePage = () => {
 
       <MovieCategory list={ListType.popular} />
       <MovieCategory list={ListType.topRated} />
-
-      <div
-        className=" w-full h-px max-w-6xl mx-auto my-20"
-        style={{
-          backgroundImage:
-            "linear-gradient(90deg, rgba(149, 131, 198, 0) 1.46%, rgba(149, 131, 198, 0.6) 40.83%, rgba(149, 131, 198, 0.3) 65.57%, rgba(149, 131, 198, 0) 107.92%)",
-        }}
-      ></div>
     </div>
   )
 }
